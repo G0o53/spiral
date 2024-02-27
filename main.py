01f45bab9f7694c4048cf00c6dc566bb93ecf13d
MatthewLizzard <matthew.kurakin@gmail.com>
2024-02-27 17:45:07 +1100

    end of program

--- main.py
+++ main.py
@@ -4,7 +4,20 @@ import turtle as t
 # the random number generator
 from random import randint
 
+
 t.bgcolor('black')
 t.speed('fast')
 t.pensize(2)
 
+for x in range(400):
+    r = randint(0, 255)
+    g = randint(0, 255)
+    b = randint(0, 255)
+
+    t.colormode(255)
+    t.pencolor(r, g, b)
+
+    t.forward(50 + x)
+    t.right(91)
+
+t.hideturtle()
